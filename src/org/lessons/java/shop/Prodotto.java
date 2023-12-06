f7ca02f8a1586e7bf4171dccc9e365e1e618a699
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-12-06 14:57:14 +0100

    inizio lavoro

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,2 +1,22 @@
-package org.lessons.java.shop;public class Prodotto {
+package org.lessons.java.shop;
+
+public class Prodotto {
+    // Attributi
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private double iva;
+
+    // Costruttori
+    public Prodotto( String nome, String descrizione, double prezzo) {
+        this.codice = codice;
+        this.nomeme = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = priceIva;
+    }
+    //Getter e Setter
+
+
 }
