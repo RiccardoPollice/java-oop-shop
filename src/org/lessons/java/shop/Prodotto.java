a3dc2a7cd85e83ac4b6d8d729801458c7e9ef675
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-12-06 15:05:22 +0100

    Aggiunto Getter e Setter

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -17,6 +17,36 @@ public class Prodotto {
         this.iva = priceIva;
     }
     //Getter e Setter
+    public int getCodice() {
+        return codice;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public double getIva() {
+        double priceIva = price * 1.22;
+        return priceIva;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+
 
 
 }
